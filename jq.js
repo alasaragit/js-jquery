8bab2469deeee86075dd065ecf52d1d5b8b6f22c
alabusa <sara.bushati@raiffeisen.al>
2022-12-14 11:16:51 +0100

    jquery

---
+++ jq.js
@@ -0,0 +1,40 @@
+function funct1() {
+
+    $("#img1").fadeToggle(3000);
+
+};
+
+function funscclick(){
+    //$(element name).action()
+    //document.getElementByTagName(element name)
+
+  //$("div").css('background-color','orange');
+  //document.getElementByID(ID)
+  $('#p1').css('font-style','italic');
+  //get class values
+  $('.para').css('font-style','italic');
+  $('.para').css('color','Blue');
+
+  $("#div1,#div2").css('color','green')
+  $("#div1,#div2").css('color','green')
+
+  $("#div1, li").css('background-color','orange');
+}
+
+
+//events javascript
+
+//function funcclick1() {
+
+//   document.getElementById("img1").style.width="500px";
+
+//};
+
+
+//events jquery
+$("button").click(funct123);
+function funct123(){
+    $("#img1").css('width','500px');
+}
+
+$()
